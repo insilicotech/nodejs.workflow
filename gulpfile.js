62603024a69474ed89aec2dbe3725bad9d301b20
Zeng Shu <ist@insilicotech.co.jp>
2018-03-08 07:30:07 +0900

    modify gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
-var gulp  = require('gulp'),
-    watch = require('gulp-watch'),
-    postcss = require('gulp-postcss'),
-    autoprefixer = require('autoprefixer'),
-    cssvars = require('postcss-simple-vars');
+var gulp          = require('gulp'),
+    watch         = require('gulp-watch'),
+    postcss       = require('gulp-postcss'),
+    autoprefixer  = require('autoprefixer'),
+    cssvars       = require('postcss-simple-vars'),
+    nested        = require('postcss-nested'),
+    imported      = require('postcss-import');
 
 gulp.task('default', function(){
   console.log('Hello Gulp!')
@@ -17,8 +19,15 @@ gulp.task('watch', function(){
   gulp.watch('./app/index.html', html);
 });
 
+// Be careful! the order of pipe processor.
 gulp.task('style', function(){
-  return gulp.src('./assets/css/main.css')
-          .pipe(postcss([autoprefixer, cssvars]))
+  return gulp.src('./app/assets/css/main.css')
+         .pipe(postcss([imported, cssvars, nested,  autoprefixer]))
          .pipe(gulp.dest('./app/css'));
+});
+
+gulp.task('watch', function(){
+  watch('./app/assets/css/**/*.css', function(){
+    gulp.start('style');
+  });
 });
